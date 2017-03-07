e6492a4c228d6411497a7e6fbe27e8cd630a05ac
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2017-03-07 13:49:34 +0900

    FIX: Validate - $sourse --> $source

--- Validate.class.php
+++ Validate.class.php
@@ -26,51 +26,51 @@ class Validate
 
 	/** ASCII
 	 *
-	 * @param string $sourse
+	 * @param string $source
 	 * @return boolean
 	 */
-	static private function _Ascii($sourse)
+	static private function _Ascii($source)
 	{
 		//	...
-		if( is_array($sourse) ){
-			$sourse = join("\n", $sourse);
+		if( is_array($source) ){
+			$source = join("\n", $source);
 		}
 
 		//	...
-		if( preg_match("/[^\x09\x0a\x0d\x20-\x7E]/", $sourse) ){
+		if( preg_match("/[^\x09\x0a\x0d\x20-\x7E]/", $source) ){
 			return true;
 		}
 	}
 
 	/** EMail
 	 *
-	 * @param string $sourse
+	 * @param string $source
 	 * @return boolean
 	 */
-	static private function _Email($sourse)
+	static private function _Email($source)
 	{
 		//	...
-		if( is_string($sourse) === false ){
+		if( is_string($source) === false ){
 			return;
 		}
 
 		//	...
-		if( strlen($sourse) === 0 ){
+		if( strlen($source) === 0 ){
 			return;
 		}
 
 		//	Do not allow alias names.
-		if( strpos($sourse, '+') !== false ){
+		if( strpos($source, '+') !== false ){
 			return true;
 		}
 
 		//	...
-		if(!strpos($sourse, '@') ){
+		if(!strpos($source, '@') ){
 			return true;
 		}
 
 		//	...
-		list($addr, $host) = explode('@', $sourse);
+		list($addr, $host) = explode('@', $source);
 
 		//	...
 		if( preg_match('/[^-_a-z0-9\.]/i', $addr, $m) ){
@@ -85,29 +85,35 @@ class Validate
 
 	/** Phone
 	 *
-	 * @param string $sourse
+	 * @param string $source
 	 * @return boolean
 	 */
-	static private function _Phone($sourse)
+	static private function _Phone($source)
 	{
-		if( preg_match('/[^-0-9\.\+\ )]/i', $sourse, $m) ){
+		if( preg_match('/[^-0-9\.\+\ )]/i', $source, $m) ){
 			return true;
 		}
 	}
 
 	/** Required
 	 *
-	 * @param  string|array $sourse
+	 * @param  string|array $source
 	 * @return boolean
 	 */
-	static private function _Required($sourse)
+	static private function _Required($source)
 	{
-		if( is_array($sourse) ){
-			if( count($sourse) === 1 ){
+		//	...
+		if( empty($source) ){
+			return true;
+		}
+
+		//	...
+		if( is_array($source) ){
+			if( count($source) === 1 ){
 				return true;
 			}
-		}else{
-			if( strlen($sourse) === 0 ){
+		}else if( is_string($source) ){
+			if( strlen($source) === 0 ){
 				return true;
 			}
 		}
@@ -115,12 +121,12 @@ class Validate
 
 	/** Sanitize
 	 *
-	 * @param  array $form   form configuration.
-	 * @param  array $sourse request by internet.
+	 * @param  array $form    form configuration.
+	 * @param  array $sources request by internet.
 	 * @param  array $errors
 	 * @return array
 	 */
-	static function Sanitize($form, $sourses)
+	static function Sanitize($form, $sources)
 	{
 		//	...
 		$errors = [];
@@ -154,7 +160,7 @@ class Validate
 				}
 
 				//	...
-				$sourse = ifset($sourses[$name], '');
+				$source = ifset($sources[$name], '');
 
 				//	...
 				switch( $validate ){
@@ -162,58 +168,58 @@ class Validate
 						break;
 
 					case 'required':
-						$fail = self::_Required($sourses[$name]);
+						$fail = self::_Required($sources[$name]);
 						break;
 
 					case 'ascii':
 					case 'english':
-						$fail = self::_Ascii($sourse);
+						$fail = self::_Ascii($source);
 						break;
 
 					case 'email':
-						$fail = self::_Email($sourse);
+						$fail = self::_Email($source);
 						break;
 
 					case 'phone':
-						$fail = self::_Phone($sourse);
+						$fail = self::_Phone($source);
 						break;
 
 					case 'short':
-						if( $len  = strlen($sourse) ){
+						if( $len  = strlen($source) ){
 							$fail = ( $len < $value) ? true: false;
 						}
 						break;
 
 					case 'long':
-						$fail = (strlen($sourse) > $value) ? true: false;
+						$fail = (strlen($source) > $value) ? true: false;
 						break;
 
 					case 'number':
-						if( strlen($sourse) ){
-							$fail = is_numeric($sourse) ? false: true;
+						if( strlen($source) ){
+							$fail = is_numeric($source) ? false: true;
 						}
 						break;
 
 					case 'min':
-						if( is_numeric($sourse) and $sourse < $value ){
+						if( is_numeric($source) and $source < $value ){
 							$fail = true;
 						}
 						break;
 
 					case 'max':
-						if( is_numeric($sourse) and $sourse > $value ){
+						if( is_numeric($source) and $source > $value ){
 							$fail = true;
 						}
 						break;
 
 					case 'positive':
-						if( is_numeric($sourse) and $sourse < 0 ){
+						if( is_numeric($source) and $source < 0 ){
 							$fail = true;
 						}
 						break;
 
 					case 'negative':
-						if( is_numeric($sourse) and $sourse > 0 ){
+						if( is_numeric($source) and $source > 0 ){
 							$fail = true;
 						}
 						break;
